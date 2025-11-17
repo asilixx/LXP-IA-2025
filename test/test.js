151084a7f50fcc2874df0328524787d8381cce38
dimc0 <dimitri.camet@efrei.net>
2025-11-17 16:19:02 +0100

    ollama

--- test/test.js
+++ test/test.js
@@ -1,7 +1,26 @@
-import ollama from 'ollama'
+const button = document.getElementById('askBtn');
+const input = document.getElementById('question');
+const output = document.getElementById('answer');
 
-const response = await ollama.chat({
-  model: 'gemma3',
-  messages: [{ role: 'user', content: 'pourquoi le diel est bleu?' }],
-})
-console.log(response.message.content)
+button.addEventListener('click', async () => {
+  const message = input.value;
+  output.textContent = 'Chargement…';
+
+  try {
+    const res = await fetch('http://localhost:11434/api/generate', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        "prompt": message,
+        "stream": false,
+        "model": "gemma3"
+       })
+    });
+
+    const data = await res.json();
+    output.textContent = data.response;
+    console.log(data)
+  } catch (err) {
+    output.textContent = 'Erreur : ' + err.message;
+  }
+});
