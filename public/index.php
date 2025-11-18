eb3f3f1961390b9a4856d685c05828d353e38daa
paull <p.lamarque33290@gmail.com>
2025-11-18 13:39:06 +0100

    front 2

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Negociation</title>
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+<body>
+    <div class="game-container">
+        <div id="title">Negociation</div>
+        <!-- <img src="../assets/background.png" alt="Background" class="background"> -->
+        <div id="monstre">
+            <img src="../assets/mechant.gif" alt="Monstre IA" class="monster">
+        </div>
+
+        <div class="hud">
+        <div class="bonheur-label"></div>
+        <div class="bonheur-bar">
+            <div class="bonheur-fill" style="width: 100%;"></div>
+        </div>
+            </div>
+        <div class="chat-bubble">
+        <p>Pose-moi une question...</p>
+        </div>
+    </div>
+</body>
+</html>
