516c2a8f98d7600bdf355c0eaa14fd18e2b7e348
dimc0 <dimitri.camet@efrei.net>
2025-11-20 16:53:26 +0100

    leaderboard

--- JS/localstorage.js
+++ JS/localstorage.js
@@ -6,12 +6,10 @@ setInterval(() => {
 
          const chrono = `${minuteglobale}:${secondeglobale < 10 ? "0" + secondeglobale : secondeglobale}`;
 
-        // sauvegarde dans le localStorage
         localStorage.setItem(playerName, chrono);
 
         console.log(`✔ Score enregistré : ${playerName} → ${chrono}`);
 
-        // empêche de sauvegarder plusieurs fois
         setPlayerName(null);
     }
 }, 500);
